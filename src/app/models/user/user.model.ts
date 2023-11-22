b77694a250912eafe74cc23f6700042b7901cdb2
ruhan <robiuzzaman4@gmail.com>
2023-11-22 22:04:32 +0600

    [DONE] create user model with types and validation

---
+++ src/app/models/user/user.model.ts
@@ -0,0 +1,59 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "./user.interface";
+
+const userSchema = new Schema<IUser>({
+  userId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  hobbies: [String, String],
+  address: {
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+// create model
+export const User = model<IUser>("User", userSchema);
